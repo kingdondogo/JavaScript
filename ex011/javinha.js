38cefd0ca10ff92723cfe4919ed02f5869a0360c
luandeolivera <51605772+kingdondogo@users.noreply.github.com>
2025-03-26 13:41:38 -0300

    up

--- ex011/javinha.js
+++ ex011/javinha.js
@@ -23,13 +23,13 @@ function verificar() {
                 img.setAttribute('src', 'bebe M.png')
             } else if (idade < 21) {
                 //jovem
-                img.setAttribute('src', 'jovem m.png')
+                img.setAttribute('src', 'jovem M.png')
             } else if (idade < 50) {
                 //adulto
                 img.setAttribute('src', 'adulto M.png')
             } else {
                 //idoso
-                img.setAttribute('src', 'idoso m.png')
+                img.setAttribute('src', 'idoso M.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
